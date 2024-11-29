a9543ba67f74d3a7f6836bbf0155382e5bb95d56
JustDevRae <ksrae165@gmail.com>
2024-11-29 12:13:12 +0900

    feat: Header 컴포넌트를 공통 레이아웃 컴포넌트로 분리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,21 @@
-import Header from "@/src/components/Header";
 import "@mantine/core/styles.css";
-import "../styles/preflight.css";
+import "@/src/styles/preflight.css";
 import "@/src/styles/globals.css";
+import "@mantine/notifications/styles.css";
+
 import theme from "@/src/styles/mantineTheme";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
-import "@mantine/notifications/styles.css";
 import type { AppProps } from "next/app";
+import Layout from "../components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
       <Notifications position="top-right" />
-      <Header />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </MantineProvider>
   );
 }
