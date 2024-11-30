94ee18edeffe4f39d7607c0ab1b6f58901fb5f0d
JustDevRae <ksrae165@gmail.com>
2024-11-30 21:43:26 +0900

    chore: SearchForm 오타 수정

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,7 +2,12 @@ import searchIcon from "@/public/ic_search.svg";
 import Image from "next/image";
 import { SearchPros } from "../types/wikiListTypes";
 
-export default function SearchFrom({ value, setValue, page, setPage }: SearchPros) {
+export default function SearchForm({
+  value,
+  setValue,
+  page,
+  setPage,
+}: SearchPros) {
   return (
     <section className="align-center mx-[20px] flex justify-center">
       <div className="relative w-[340px] md:w-[700px] lg:w-[860px]">
@@ -20,7 +25,13 @@ export default function SearchFrom({ value, setValue, page, setPage }: SearchPro
           className="m-auto h-[45px] w-full rounded-[20px] bg-gray-100 py-[20px] pl-[50px] pr-[15px] outline-none"
         />
         <div className="absolute left-[20px] top-1/2 -translate-y-1/2 transform">
-          <Image src={searchIcon} alt="검색 아이콘" width={22} height={22} draggable="false" />
+          <Image
+            src={searchIcon}
+            alt="검색 아이콘"
+            width={22}
+            height={22}
+            draggable="false"
+          />
         </div>
       </div>
     </section>
--- src/pages/wikilist/index.tsx
+++ src/pages/wikilist/index.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { Article } from "@/src/types/wikiListTypes";
 import Pagination from "@/src/components/Pagination";
 import NoSearchImage from "@/public/img_no_search.webp";
-import SearchFrom from "@/src/components/SearchFrom";
+import SearchForm from "@/src/components/SearchForm";
 
 export default function WikiList() {
   const [articles, setArticles] = useState<Article[]>([]);
@@ -16,7 +16,9 @@ export default function WikiList() {
   useEffect(() => {
     async function fetchArticles() {
       try {
-        const response = await instance.get(`/profiles`, { params: { name: value, page } });
+        const response = await instance.get(`/profiles`, {
+          params: { name: value, page },
+        });
         if (Array.isArray(response.data.list)) {
           setArticles(response.data.list);
           setTotalPages(Math.ceil(response.data.totalCount / 3));
@@ -35,17 +37,30 @@ export default function WikiList() {
     fetchArticles();
   }, [page, value]);
 
-  const searchResults = value ? articles.filter((article) => article.name.toLowerCase().includes(value.toLowerCase())) : articles;
+  const searchResults = value
+    ? articles.filter((article) =>
+        article.name.toLowerCase().includes(value.toLowerCase()),
+      )
+    : articles;
 
   return (
     <div className="m-auto mt-[40px] w-[350px] md:mt-[60px] md:w-[700px] lg:mt-[80px] lg:w-[860px]">
       <main>
         <div>
-          <SearchFrom value={value} setValue={setValue} page={page} setPage={setPage} />
+          <SearchForm
+            value={value}
+            setValue={setValue}
+            page={page}
+            setPage={setPage}
+          />
           <section className="m-auto my-4 w-[340px] px-4 text-[16px] font-[400] text-gray-400 md:w-[700px] lg:w-[860px]">
             {value && searchResults.length > 0 ? (
               <p>
-                &quot;{value}&quot;님을 총<span className="text-green-200">&nbsp;{searchResults.length}</span>명 찾았습니다.
+                &quot;{value}&quot;님을 총
+                <span className="text-green-200">
+                  &nbsp;{searchResults.length}
+                </span>
+                명 찾았습니다.
               </p>
             ) : (
               <br />
@@ -54,21 +69,41 @@ export default function WikiList() {
         </div>
         <section className="my-20 h-[470px]">
           {searchResults.length > 0 ? (
-            searchResults.map((article) => <UserCard key={article.id} articles={[article]} />)
+            searchResults.map((article) => (
+              <UserCard key={article.id} articles={[article]} />
+            ))
           ) : (
             <div>
               <div className="flex justify-center py-[32px] text-[18px] font-[500] text-gray-400 md:text-[20px] lg:text-[20px]">
                 <div className="flex-none">&quot;</div>
-                <div className="max-w-[100px] flex-none truncate md:max-w-[200px] lg:max-w-[350px]">{value}</div>
-                <div className="flex-none">&quot;과&#47;와 일치하는 검색 결과가 없어요.</div>
+                <div className="max-w-[100px] flex-none truncate md:max-w-[200px] lg:max-w-[350px]">
+                  {value}
+                </div>
+                <div className="flex-none">
+                  &quot;과&#47;와 일치하는 검색 결과가 없어요.
+                </div>
               </div>
               <div className="m-auto w-[100px] md:w-[144px] lg:w-[144px]">
-                <Image src={NoSearchImage} alt="검색 결과 없음 이미지" draggable="false" />
+                <Image
+                  src={NoSearchImage}
+                  alt="검색 결과 없음 이미지"
+                  draggable="false"
+                />
               </div>
             </div>
           )}
         </section>
-        <footer>{searchResults.length > 0 ? <Pagination totalPages={totalPages} currentPage={page} onPageChange={(newPage) => setPage(newPage)} /> : <br />}</footer>
+        <footer>
+          {searchResults.length > 0 ? (
+            <Pagination
+              totalPages={totalPages}
+              currentPage={page}
+              onPageChange={(newPage) => setPage(newPage)}
+            />
+          ) : (
+            <br />
+          )}
+        </footer>
       </main>
     </div>
   );
